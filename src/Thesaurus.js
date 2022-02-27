ced245ef7073ea51b427110fc1fe4e687f4ce1a8
Lellien <ibexcomplex@gmail.com>
2022-02-27 17:05:58 +0000

    Fixed bugs in Thesaurus component
    - specified .length >0 as condition for synonyms/antonyms so they are no longer rendered from an empty array.
    - added wrap to fix text overflow in thesaurus lists.

--- src/Thesaurus.js
+++ src/Thesaurus.js
@@ -9,34 +9,44 @@ export default function Thesausus(props) {
     props.newResults(newWord);
   }
 
-  let synonyms = props.definition.synonyms ? (
-    <div className="Thesaurus-synonyms">
-      <span className="Thesaurus-similar">Similar:</span>
-      <ul className="Thesaurus-synonyms-list">
-        {props.definition.synonyms.map((synonym, index) => {
-          return (
-            <li className="Thesaurus-synonym" key={index} onClick={searchAgain}>
-              {synonym}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  ) : null;
-  let antonyms = props.definition.antonyms ? (
-    <div className="Thesaurus-antonyms">
-      <span className="Thesaurus-opposite">Opposite:</span>
-      <ul className="Thesaurus-antonyms-list">
-        {props.definition.antonyms.map((antonym, index) => {
-          return (
-            <li className="Thesaurus-antonym" key={index} onClick={searchAgain}>
-              {antonym}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  ) : null;
+  let synonyms =
+    props.definition.synonyms.length > 0 ? (
+      <div className="Thesaurus-synonyms">
+        <span className="Thesaurus-similar">Similar:</span>
+        <ul className="Thesaurus-synonyms-list">
+          {props.definition.synonyms.map((synonym, index) => {
+            return (
+              <li
+                className="Thesaurus-synonym"
+                key={index}
+                onClick={searchAgain}
+              >
+                {synonym}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    ) : null;
+  let antonyms =
+    props.definition.antonyms.length > 0 ? (
+      <div className="Thesaurus-antonyms">
+        <span className="Thesaurus-opposite">Opposite:</span>
+        <ul className="Thesaurus-antonyms-list">
+          {props.definition.antonyms.map((antonym, index) => {
+            return (
+              <li
+                className="Thesaurus-antonym"
+                key={index}
+                onClick={searchAgain}
+              >
+                {antonym}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    ) : null;
 
   return (
     <div className="Thesaurus">
